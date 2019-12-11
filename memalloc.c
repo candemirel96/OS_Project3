8a8825c6766ffe710cdf539d690bbf4b0a572ee7
pegahsoltani <34623821+pegahsoltani@users.noreply.github.com>
2019-12-11 22:19:02 +0300

    Add files via upload
    added from the source.
    Errors are conflicting types(?)

--- memalloc.c
+++ memalloc.c
@@ -1,37 +1,98 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "memalloc.h"
 #include <stdbool.h>
+#include <pthread.h>
+// #include "memalloc.h"
 
-void* cpointer;
-int   csize;
-int   cmethod; 
+// global variables
+void* chunkPointer;
+int chunkSize;
+int chunkMethod;
+pthread_mutex_t global_malloc_lock;
 
-typedef struct memory_ref{
-    //char* name;
-    int size;
-    void* adress;
-    struct memory_ref* next;
+struct header_t{
+  size_t size;
+  struct header_t* next;
+  unsigned is_free;
 };
 
+typedef char ALIGN[16];
+union header {
+	struct {
+		size_t size;
+		unsigned is_free;
+		union header *next;
+	} s;
+	ALIGN stub;
+};
+typedef union header header_t;
+
+header_t *head, *tail; // global
+
+int mem_init(void* chunkpointer, int chunksize, int method){
+  // just do it for the first fit algorthm
+  if(method == 0){
+    printf("\nUsing first fit algorithm!\n");
+
+  }
+  printf("\ninit called\n");
+  return(0); // case of success
+}
 
 
-int mem_init (void *chunkpointer, int chunksize, int method){
-    printf("initcalled\n");
-    cpointer=chunkpointer;
-    csize=chunksize;
-    cmethod=method;
-    return (0);// if success
-    }
-void *mem_allocate (int objectsize){
-    printf("alloc called\n");
-    return (NULL);// if not success
-    }
-void mem_free(void *objectptr){
-    printf("free called\n");
-    return;
-    }
-void mem_print (void){
-    printf("print called\n");
-    return;
-    }
+void *mem_allocate(size_t size){
+  size_t total_size; // initiate a size_t for total size
+  void* block;
+  block = sbrk(size);
+  header_t* header;
+  if(block == (void*)-1){
+    return NULL; // case if failure
+  }
+
+  pthread_mutex_lock(&global_malloc_lock);
+  header =(header_t*)get_free_block(size);
+  if (header) {
+		header->s.is_free = 0;
+		pthread_mutex_unlock(&global_malloc_lock);
+		return (void*)(header + 1); //
+	}
+  total_size = sizeof(header_t) + size;
+  block = sbrk(total_size);
+  if (block == (void*) -1) {
+  pthread_mutex_unlock(&global_malloc_lock);
+  return NULL;
+  }
+
+  header = block;
+	header->s.size = size;
+	header->s.is_free = 0;
+	header->s.next = NULL;
+  if (!head)
+		head = header;
+	if (tail)
+		tail->s.next = header;
+	tail = header;
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void*)(header + 1);
+}
+
+header_t* get_free_block(size_t size)
+{
+	header_t *curr = head;
+	while(curr) {
+		if (curr->s.is_free && curr->s.size >= size)
+			return curr;
+		curr = curr->s.next;
+	}
+	return NULL;
+}
+
+void mem_free(void* objectptr){
+  printf("\nfree called\n");
+  return;
+}
+
+void mem_print(void){
+  printf("\nprint called\n");
+  return;
+}
--- memalloc.h
+++ memalloc.h
@@ -1,10 +1,10 @@
 #ifndef MEMALLOC_H
 #define MEMALLOC_H
-int mem_init(void *, int, int);
-void *mem_allocate(int);
-void mem_free(void *);
-void mem_print (void); 
-#define FIRST_FIT 0
-#define BEST_FIT 1
-#define WORST_FIT 2
+
+int mem_init(void*, int, int);
+void* mem_allocate(size_t);
+void mem_free(void*);
+void mem_print(void);
+header_t *get_free_block(size_t);
+
 #endif
