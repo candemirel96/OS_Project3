2525e951aae813031c777830781bb62d991273e3
pegahsoltani <pegsoltani@gmail.com>
2019-12-15 15:21:23 +0300

    Added arg V from the user

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 #include "memalloc.h"
 
 int main(int argc, char* argv[]){
-  int initial_chunkSize = 1024 * 1024;
+  size = atoi(argv[1]);// unit is in KB
+  int initial_chunkSize = 1024 * size;
   void* initial_chunkPointer = malloc(initial_chunkSize);
   printf("A 1 kB memory chunk is created at %p ending at %p\n"
 	 , initial_chunkPointer,
 	 (void*)((char*)initial_chunkPointer + initial_chunkSize));
-  
+
   mem_init(initial_chunkPointer, initial_chunkSize, 0);
-  
+
   void* testAddr1 = mem_allocate(512*1024);
   void* testAddr2 = mem_allocate(64*1024);
   void* testAddr3 = mem_allocate(128*1024);
